2f5649b853d8a9835e2db9ab8d84f5c386d26a1e
Mikhail Ivanov <m.ivanov0427@gmail.com>
2024-03-15 23:41:47 +0300

    feat: automate package.json updating

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,47 @@
-import { error } from "console";
 import fs from "fs/promises";
 import { themes } from "./themes";
 import { toKebabCase } from "./utils";
 
 async function main() {
   try {
+    await fs.rm("./themes", { recursive: true, force: true });
     await fs.mkdir("./themes", { recursive: true });
-  } catch {
+
+    const packageJsonPath = "./package.json";
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath, "utf8"));
+    packageJson.contributes = packageJson.contributes || {};
+    packageJson.contributes.themes = [];
+
+    for (const theme of themes) {
+      try {
+        const fileName = toKebabCase(
+          `${theme.colors.name}-${theme.tokens.name}`
+        );
+        const path = `./themes/${fileName}.json`;
+        const themeCompiled = {
+          name: `${theme.colors.name} (${theme.tokens.name})`,
+          colors: theme.colors.colors,
+          tokenColors: theme.tokens.tokens,
+        };
+
+        console.log(`Writing theme to ${path}`);
+        await fs.writeFile(path, JSON.stringify(themeCompiled, null, 2));
+
+        packageJson.contributes.themes.push({
+          label: themeCompiled.name,
+          uiTheme: "vs-dark",
+          path: `./themes/${fileName}.json`,
+        });
+      } catch (error) {
+        console.error(error);
+        process.exit(1);
+      }
+    }
+
+    await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
+  } catch (error) {
     console.error(error);
     process.exit(1);
-  }
-
-  for (const theme of themes) {
-    try {
-      const fileName = toKebabCase(`${theme.colors.name}-${theme.tokens.name}`);
-      const path = `./themes/${fileName}.json`;
-
-      const themeCompiled = {
-        name: `${theme.colors.name} (${theme.tokens.name})`,
-        colors: theme.colors.colors,
-        tokenColors: theme.tokens.tokens,
-      };
-
-      console.log(path);
-
-      await fs.writeFile(path, JSON.stringify(themeCompiled, null, 2));
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
   }
 }
 
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -6,5 +6,5 @@ export const themes: Theme[] = [
   {
     tokens: xcodeTokens,
     colors: trueVibrantColors,
-  },
+  }
 ] as const;
